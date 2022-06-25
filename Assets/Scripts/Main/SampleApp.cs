14e7a83ef6418c48322ad0388ed1202d9786d817
杰克有茶 <chenwansal1@163.com>
2022-06-26 01:32:36 +0800

    <feature> 新增: Matrix4x4Fixed *

--- Assets/Scripts/Main/SampleApp.cs
+++ Assets/Scripts/Main/SampleApp.cs
@@ -15,39 +15,33 @@ namespace MySampleApp {
 
         void Awake() {
 
-            Fixed64 fp = 3;
+            FixedTest();
 
-            var a = new Vector3Fixed(3, 4, 5);
-            var b = new Vector3Fixed(8, 9, 10);
-            Fixed64 d = Vector3Fixed.Dot(a, b);
-            Debug.Log(d);
-            Debug.Log(a * b);
-            Debug.Log(a.x.ToString());
-            Debug.Log(fp.ToString());
-
-            float dot = Vector3.Dot(new Vector3(3, 4, 5), new Vector3(8, 9, 10));
-            Debug.Log(dot);
-
-            Matrix4x4 m = new Matrix4x4(
-                new Vector4(1, 2, 3, 4),
-                new Vector4(5, 6, 7, 8),
-                new Vector4(9, 10, 11, 12),
-                new Vector4(13, 14, 15, 16)
-            );
-
-            // Matrix4x4 mt = Matrix4x4.Translate(new Vector3(2, 3, 4));
-            // Debug.Log(mt.ToString());
-
-            Matrix4x4 mt2 = Matrix4x4.Transpose(m);
-            Debug.Log(m.ToString());
-            Debug.Log(mt2.ToString());
+            NormalTest();
 
         }
 
-        void Update() {
-            if (Input.GetKeyUp(KeyCode.Space)) {
-                Physics.Simulate(0.01f);
-            }
+        void FixedTest() {
+
+            Matrix4x4Fixed lhs = Matrix4x4Fixed.ScaleByVector(new Vector3Fixed(1, 2, 1)) * Matrix4x4Fixed.TranslateByVector(new Vector3Fixed(1, 1, 1));
+            Matrix4x4Fixed rhs = Matrix4x4Fixed.TranslateByVector(new Vector3Fixed(2, 3, 4));
+            Matrix4x4Fixed mt = lhs * rhs;
+            Debug.Log(lhs);
+            Debug.Log(mt);
+
+        }
+
+        void NormalTest() {
+
+            Matrix4x4 lhs = Matrix4x4.Scale(new Vector3(1, 2, 1)) * Matrix4x4.Translate(new Vector3(1, 1, 1));
+            Matrix4x4 rhs = Matrix4x4.Translate(new Vector3(2, 3, 4));
+            Matrix4x4 mt = lhs * rhs;
+            Debug.Log(lhs.ToString());
+            Debug.Log(mt);
+
+            Matrix4x4 rot = Matrix4x4.Rotate(Quaternion.Euler(0, 0, 90));
+            Debug.Log(rot);
+
         }
 
     }
--- Assets/com.jackwithtea.jackframe/LowLevel_Independence/DefiniteMath/Matrix4x4Fixed.cs
+++ Assets/com.jackwithtea.jackframe/LowLevel_Independence/DefiniteMath/Matrix4x4Fixed.cs
@@ -9,6 +9,13 @@ namespace JackFrame.DefiniteMath {
         public Fixed64 m31, m32, m33, m34;
         public Fixed64 m41, m42, m43, m44;
 
+        public readonly static Matrix4x4Fixed identity = new Matrix4x4Fixed(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        );
+
         public Matrix4x4Fixed(Vector4Fixed row1, Vector4Fixed row2, Vector4Fixed row3, Vector4Fixed row4) {
             this.m11 = row1.x; this.m12 = row1.y; this.m13 = row1.z; this.m14 = row1.w;
             this.m21 = row2.x; this.m22 = row2.y; this.m23 = row2.z; this.m24 = row2.w;
@@ -16,11 +23,30 @@ namespace JackFrame.DefiniteMath {
             this.m41 = row4.x; this.m42 = row4.y; this.m43 = row4.z; this.m44 = row4.w;
         }
 
-        public static Matrix4x4Fixed CreateScaleByVector(Vector3Fixed scales) {
-            return CreateScale(scales.x, scales.y, scales.z);
+        public Matrix4x4Fixed(
+            Fixed64 m11, Fixed64 m12, Fixed64 m13, Fixed64 m14,
+            Fixed64 m21, Fixed64 m22, Fixed64 m23, Fixed64 m24,
+            Fixed64 m31, Fixed64 m32, Fixed64 m33, Fixed64 m34,
+            Fixed64 m41, Fixed64 m42, Fixed64 m43, Fixed64 m44
+        ) {
+            this.m11 = m11; this.m12 = m12; this.m13 = m13; this.m14 = m14;
+            this.m21 = m21; this.m22 = m22; this.m23 = m23; this.m24 = m24;
+            this.m31 = m31; this.m32 = m32; this.m33 = m33; this.m34 = m34;
+            this.m41 = m41; this.m42 = m42; this.m43 = m43; this.m44 = m44;
         }
 
-        public static Matrix4x4Fixed CreateScale(Fixed64 x, Fixed64 y, Fixed64 z) {
+        public override string ToString() {
+            return $"{m11} {m12} {m13} {m14}\r\n"
+                  +$"{m21} {m22} {m23} {m24}\r\n"
+                  +$"{m31} {m32} {m33} {m34}\r\n"
+                  +$"{m41} {m42} {m43} {m44}";
+        }
+
+        public static Matrix4x4Fixed ScaleByVector(Vector3Fixed scales) {
+            return Scale(scales.x, scales.y, scales.z);
+        }
+
+        public static Matrix4x4Fixed Scale(Fixed64 x, Fixed64 y, Fixed64 z) {
             Matrix4x4Fixed mt = new Matrix4x4Fixed(
                 new Vector4Fixed(x, 0, 0, 0),
                 new Vector4Fixed(0, y, 0, 0),
@@ -58,16 +84,46 @@ namespace JackFrame.DefiniteMath {
             return value;
         }
 
-        public static Matrix4x4Fixed CreateTranslateByVector(Vector3Fixed translation) {
-            return CreateTranslate(translation.x, translation.y, translation.z);
+        public static Matrix4x4Fixed TranslateByVector(Vector3Fixed translation) {
+            return Translate(translation.x, translation.y, translation.z);
         }
 
-        public static Matrix4x4Fixed CreateTranslate(Fixed64 x, Fixed64 y, Fixed64 z) {
+        public static Matrix4x4Fixed Translate(Fixed64 x, Fixed64 y, Fixed64 z) {
             Matrix4x4Fixed mt = new Matrix4x4Fixed(
                 new Vector4Fixed(1, 0, 0, x),
                 new Vector4Fixed(0, 1, 0, y),
                 new Vector4Fixed(0, 0, 1, z),
                 new Vector4Fixed(0, 0, 0, 1)
+            );
+            return mt;
+        }
+
+        public static Matrix4x4Fixed operator *(Matrix4x4Fixed lhs, Matrix4x4Fixed rhs) {
+            Matrix4x4Fixed mt = new Matrix4x4Fixed(
+                new Vector4Fixed(
+                    lhs.m11 * rhs.m11 + lhs.m12 * rhs.m21 + lhs.m13 * rhs.m31 + lhs.m14 * rhs.m41,
+                    lhs.m11 * rhs.m12 + lhs.m12 * rhs.m22 + lhs.m13 * rhs.m32 + lhs.m14 * rhs.m42,
+                    lhs.m11 * rhs.m13 + lhs.m12 * rhs.m23 + lhs.m13 * rhs.m33 + lhs.m14 * rhs.m43,
+                    lhs.m11 * rhs.m14 + lhs.m12 * rhs.m24 + lhs.m13 * rhs.m34 + lhs.m14 * rhs.m44
+                ),
+                new Vector4Fixed(
+                    lhs.m21 * rhs.m11 + lhs.m22 * rhs.m21 + lhs.m23 * rhs.m31 + lhs.m24 * rhs.m41,
+                    lhs.m21 * rhs.m12 + lhs.m22 * rhs.m22 + lhs.m23 * rhs.m32 + lhs.m24 * rhs.m42,
+                    lhs.m21 * rhs.m13 + lhs.m22 * rhs.m23 + lhs.m23 * rhs.m33 + lhs.m24 * rhs.m43,
+                    lhs.m21 * rhs.m14 + lhs.m22 * rhs.m24 + lhs.m23 * rhs.m34 + lhs.m24 * rhs.m44
+                ),
+                new Vector4Fixed(
+                    lhs.m31 * rhs.m11 + lhs.m32 * rhs.m21 + lhs.m33 * rhs.m31 + lhs.m34 * rhs.m41,
+                    lhs.m31 * rhs.m12 + lhs.m32 * rhs.m22 + lhs.m33 * rhs.m32 + lhs.m34 * rhs.m42,
+                    lhs.m31 * rhs.m13 + lhs.m32 * rhs.m23 + lhs.m33 * rhs.m33 + lhs.m34 * rhs.m43,
+                    lhs.m31 * rhs.m14 + lhs.m32 * rhs.m24 + lhs.m33 * rhs.m34 + lhs.m34 * rhs.m44
+                ),
+                new Vector4Fixed(
+                    lhs.m41 * rhs.m11 + lhs.m42 * rhs.m21 + lhs.m43 * rhs.m31 + lhs.m44 * rhs.m41,
+                    lhs.m41 * rhs.m12 + lhs.m42 * rhs.m22 + lhs.m43 * rhs.m32 + lhs.m44 * rhs.m42,
+                    lhs.m41 * rhs.m13 + lhs.m42 * rhs.m23 + lhs.m43 * rhs.m33 + lhs.m44 * rhs.m43,
+                    lhs.m41 * rhs.m14 + lhs.m42 * rhs.m24 + lhs.m43 * rhs.m34 + lhs.m44 * rhs.m44
+                )
             );
             return mt;
         }
